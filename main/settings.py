40ba4cf48442ef7f3d4077af7e0f8bbb46d8a92f
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-28 20:10:41 +0000

    troubleshooting and now I have 3 ports open

--- main/settings.py
+++ main/settings.py
@@ -61,15 +61,20 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],  # Ensure this points to the correct 'templates' directory
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                ...
-            ],
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+],
+
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
