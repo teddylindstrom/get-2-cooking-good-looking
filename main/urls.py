2cb7b4091fb9773f723489bad4a0147c7bf54964
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-30 19:09:36 +0000

    made some alterations to the code, trying to figure out what's wrong by checking them in ChatGPT

--- main/urls.py
+++ main/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Note the trailing slash
-    path('', include('home.urls')),  # Remove the space
+    path('', include('home.urls')),  # Directs to the home app's URLs
 ]
